72ecfded4c88e7608d48bebcec17fc7432565649
Kacper Bujak <kaccpper2@gmail.com>
2023-08-03 13:07:42 +0200

    light/dark mode icon

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.scss';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [isDark, setIsDark ] = useState(false);
+
   return (
     <>
-      <Header />
+      <Header isDark={isDark} setIsDark={setIsDark}/>
       <Main />
       <Footer />
     </>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,20 @@
 import webDevIcon from '../../assets/webDevIcon.svg';
-import menuIcon from '../../assets/menuIcon.svg';
+import lightMode from '../../assets/light_mode.svg';
+import darkMode from '../../assets/dark_mode.svg';
 import './Header.scss';
 
-const Header = () => {
+const Header = ({isDark, setIsDark}) => {
   return (
     <header className="header">
       <div className="logo">
         <img src={webDevIcon} alt="logo" className="logoIcon" />
       </div>
-      <button className='menu'>
-        <img src={menuIcon} alt="menu" className='menuIcon'/>
+      <button className='darkMode'>
+        {
+         isDark ? 
+         <img src={darkMode} alt="darkModeIcon" className='darkModeIcon'/> :
+         <img src={lightMode} alt="darkModeIcon" className='darkModeIcon'/>
+        }
       </button>
     </header>
   )
