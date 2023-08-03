d0c7557841fb9a943fe66fa4d048ae8d549eb516
Kacper Bujak <kaccpper2@gmail.com>
2023-08-03 12:52:09 +0200

    design for mobile and desktop

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -3,6 +3,7 @@ const Project = ({src, title}) => {
     <div className="project">
       <div className="projectImageDiv"><img src={src} alt={src} className='projectImage'/></div>
       <div className="projectTitle">{title}</div>
+      <div className="projectText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui non nobis quo, rerum molestias libero at atque voluptatem sit, harum hic, ab ut laudantium corrupti fuga dolor. Corporis, cum assumenda.</div>
     </div>
   )
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,16 +5,23 @@ import youtube from '../../assets/youtube.jpg';
 import cart from '../../assets/computer-shop.jpg';
 import memory from '../../assets/memory-card.jpg';
 import waldo from '../../assets/wheres-waldo.jpg';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+
 
 const Projects = () => {
+
   return (
     <section className="projects">
       <div className="projectsTitle">Projects</div>
-      <Project title={'Youtube-copy'} src={youtube}/>
-      <Project title={'Shopping Cart'} src={cart}/>
-      <Project title={'Memory Card'} src={memory}/>
-      <Project title={'Battleship'} src={battleship}/>
-      <Project title={"Where's Waldo"} src={waldo}/>
+        <div className="projectsGrid">
+          <Project title={'Youtube-copy'} src={youtube}/>
+          <Project title={'Shopping Cart'} src={cart}/>
+          <Project title={'Memory Card'} src={memory}/>
+          <Project title={'Battleship'} src={battleship}/>
+          <Project title={"Where's Waldo"} src={waldo}/>
+        </div>
     </section>
   )
 }
